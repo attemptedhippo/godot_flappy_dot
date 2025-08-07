e1cd337754a95ef7fdcf225e5e12a37bc5b3aaad
Devon <attemptedhippo@gmail.com>
2025-08-07 18:38:46 -0400

    bird controls and main menu finished, starting on pipe generation

--- scenes/bird/bird.gd
+++ scenes/bird/bird.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 
-const GRAVITY : int = 1000
-const MAX_VEL : int = 600
-const FLAP_SPEED : int = -500
+const GRAVITY : int = 1000#500
+const MAX_VEL : int = 500#600(debug:200)
+const FLAP_SPEED : int = -400#-500(debug:-200)
 var flying : bool = false
 var falling : bool = false
 const START_POS = Vector2(100, 400)
+var dead : bool = false
 
 func _read():
 	reset()
@@ -14,12 +15,15 @@ func _read():
 func reset():
 	falling = false
 	flying = false
+	dead = false
 	position = START_POS
 	set_rotation(0)
 
 func _physics_process(delta: float) -> void:
-	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():s
-	
+	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if dead:
+		return
+		
 	if flying or falling:
 		velocity.y += GRAVITY * delta
 	
@@ -27,7 +31,7 @@ func _physics_process(delta: float) -> void:
 			velocity.y = MAX_VEL
 		
 		if flying:
-			set_rotation(deg_to_rad(velocity.y * 0.5))
+			set_rotation(deg_to_rad(velocity.y * 0.05))
 			$AnimatedSprite2D.play()
 		elif falling:
 			set_rotation(PI/2)
@@ -36,6 +40,10 @@ func _physics_process(delta: float) -> void:
 		move_and_collide(velocity * delta)
 	else:
 		$AnimatedSprite2D.stop()
+	
+	if position.y >= get_window().size.y:
+		get_parent().bird_crash()
+		dead = true
 
 func flap():
 	velocity.y = FLAP_SPEED
--- scenes/main.gd
+++ scenes/main.gd
@@ -10,9 +10,12 @@ var ground_height : int
 var pipes : Array
 const PIPE_DELAY : int = 100
 const PIPE_RANGE : int = 200
+var debounce_max : float = 3
+var debounce : float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	screen_size = get_window().size
 	new_game()
 	
 
@@ -21,8 +24,48 @@ func new_game():
 	game_over = false
 	score = 0
 	scroll = 0
+	$Bird.reset()
+	$background.region_rect.position.x = 0
+	$game_over.visible = false
 
+func _input(event: InputEvent) -> void:
+	if game_over and debounce > 0: #== false: #we learned about guard clauses
+		return
+	
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if game_running == false:
+				if game_over:
+					new_game()
+				start_game()
+			else:
+				if $Bird.flying:
+					$Bird.flap()
+
+func start_game():
+	game_running = true
+	$Bird.flying = true
+	$Bird.flap()
+
+func bird_crash():
+	game_over = true
+	game_running = false
+	debounce = debounce_max
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if game_over:
+		$game_over.visible = true
+		if debounce > 0:
+			debounce -= delta
+		return
+		
+	if game_running:
+		scroll += SCROLL_SPEED
+		
+		if scroll >= screen_size.x:
+			scroll = 0
+			
+		$background.region_rect.position.x = scroll
+	
+	
